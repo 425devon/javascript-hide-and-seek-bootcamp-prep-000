44e3b755a8e2e63bc18dccfb8fa8603fbe0028aa
Devon Langendoerfer <425devon@gmail.com>
2017-11-21 23:50:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function getFirstSelector(selector){
   const lis = document.querySelector(selector);
   return lis;
+};
+
+function nestedTarget(target){
+  var find = document.getElementById('nested');
+
+  return find;
 }
