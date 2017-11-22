59175d2b2d8b8682f68f11bb2ee9602b63c40b6d
Devon Langendoerfer <425devon@gmail.com>
2017-11-22 00:04:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function increaseRankBy(n){
   const lis =document.getElementById('app').querySelectorAll('ul.ranked-list li')
 
 for (let i = 0; i < lis.length; i++) {
-  lis[i].innerHTML = (i + 1).toString()
+  lis[i].innerHTML += n.toString()
 }
 }
